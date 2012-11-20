3b924de03dbb77d2c5bad5a7d754ad436bdfb161
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-11-20 12:21:08 +0100

    videodecoder: Return the proportion directly

--- gst-libs/gst/video/gstvideodecoder.h
+++ gst-libs/gst/video/gstvideodecoder.h
@@ -358,8 +358,7 @@ GstVideoCodecState *gst_video_decoder_get_output_state (GstVideoDecoder *decoder
 GstClockTimeDiff gst_video_decoder_get_max_decode_time (GstVideoDecoder *decoder,
 							GstVideoCodecFrame *frame);
 
-gboolean         gst_video_decoder_get_qos_proportion (GstVideoDecoder * decoder,
-                                                       gdouble         * proportion);
+gdouble          gst_video_decoder_get_qos_proportion (GstVideoDecoder * decoder);
 
 GstFlowReturn    gst_video_decoder_finish_frame (GstVideoDecoder *decoder,
 						 GstVideoCodecFrame *frame);
